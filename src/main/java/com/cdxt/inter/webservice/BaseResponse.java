3a60a2206b7162dab2e1d5b78cc9f0644805e9c1
tangxiaohui <nankaitang@sian.cn>
2020-06-01 21:26:40 +0800

    response

---
+++ src/main/java/com/cdxt/inter/webservice/BaseResponse.java
@@ -0,0 +1,28 @@
+package com.cdxt.inter.webservice;
+
+import com.cdxt.inter.enums.IFStateEnum;
+import com.cdxt.inter.enums.ReturnCodeEnum;
+import lombok.Data;
+
+/**
+ * @Description: 接口返回类
+ * @Author: tangxiaohui
+ * @CreateDate: 2020/6/1 0001 20:24
+ * @Copyright: Copyright (c) 2020
+ * @Company: 成都信通网易医疗科技发展有限公司
+ * @Version: 1.0
+ */
+public class BaseResponse<T> {
+    private IFStateEnum state;
+    private ReturnCodeEnum returnCode;
+    private String message;
+    private T result;
+
+    public BaseResponse(){}
+    public BaseResponse(IFStateEnum state, ReturnCodeEnum returnCode, String message, T result) {
+        this.state = state;
+        this.returnCode = returnCode;
+        this.message = message;
+        this.result = result;
+    }
+}
